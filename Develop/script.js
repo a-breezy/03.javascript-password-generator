4b4a8b1d9fb46b8316765e7c4e9083187c81e2ef
a.breezy <ambrose.wilkins@gmail.com>
2021-10-19 11:48:37 -0400

    adds password length logic

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,107 @@
+// Array of special characters to be included in password
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.'
+];
+// Array of numeric characters to be included in password
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+// Array of lowercase characters to be included in password
+var lowerCasedCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
+// Array of uppercase characters to be included in password
+var upperCasedCharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+];
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+// generate password
+function generatePassword() {
+  // Do stuff
+  const totalLength = prompt("How many characters would you like to use? (Between 8 and 128)");
+  if(totalLength >= 8 || totalLength <= 128) {
+    // tell user their password length
+    alert("Your new password will be ${totalLength} characters long.");
+  }
+  else {
+    // tell user their password is not an acceptable length
+    alert("Your password is not an acceptable length.");
+  }
+
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -11,4 +113,7 @@ function writePassword() {
 }
 
 // Add event listener to generate button
+// GIVEN I need a new, secure password
+// WHEN I click the button to generate a password
+// THEN I am presented with a series of prompts for password criteria
 generateBtn.addEventListener("click", writePassword);
